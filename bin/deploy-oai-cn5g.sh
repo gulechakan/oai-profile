04b0bcd5fceab184b42fa1c7394cb3a6bbe08ef5
Hakan Gulec <hakangulec.boun@gmail.com>
2026-01-30 20:00:11 -0500

    Initial commit

---
+++ bin/deploy-oai-cn5g.sh
@@ -0,0 +1,64 @@
+
+# Exit on error
+set -e
+
+# Create directory to deploy OAI and mount extra disk space
+cd /
+sudo mkdir mydata
+sudo /usr/local/etc/emulab/mkextrafs.pl -f /mydata
+
+# change the ownership of this new space
+username=$(whoami)
+groupname=$(id -gn)
+
+sudo chown $username:$groupname mydata
+chmod 775 mydata
+
+# verify the result
+ls -ld mydata
+
+# Define working directory for CN5G
+CN_DIR="/mydata/oai-cn5g"
+
+# Enable packet forwarding
+sudo sysctl net.ipv4.conf.all.forwarding=1
+sudo iptables -P FORWARD ACCEPT
+
+# Setup the node for deployment
+function setup_oai_node {
+    # Install docker, docker compose, wireshark/tshark
+    echo setting up oai node
+    sudo apt-get update && sudo apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      docker.io \
+      docker-compose-v2 \
+      gnupg \
+      lsb-release
+
+    sudo add-apt-repository -y ppa:wireshark-dev/stable
+    echo "wireshark-common wireshark-common/install-setuid boolean false" | sudo debconf-set-selections
+
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo apt-get install -y \
+        wireshark \
+        tshark
+
+    sudo systemctl enable docker
+    sudo usermod -aG docker $USER
+
+    printf "installing compose"
+    until sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose; do
+        printf '.'
+        sleep 2
+    done
+
+    sudo chmod +x /usr/local/bin/docker-compose
+
+    sudo sysctl net.ipv4.conf.all.forwarding=1
+    sudo iptables -P FORWARD ACCEPT
+
+    echo setting up oai node... done.
+}
+
+setup_oai_node
---
+++ profile.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import os
+
+import geni.portal as portal
+import geni.rspec.pg as rspec
+import geni.rspec.igext as IG
+import geni.rspec.emulab.pnext as PN
+import geni.rspec.emulab.spectrum as spectrum
+
+
+tourDescription = """
+### OAI 5G E2E Low Latency 
+
+This profile instantiates an experiment for testing OAI 5G end-to-end low latency service.
+
+- A d430 compute node to host the OAI 5G CN, OAI 5G RAN, and NR UE(s).
+
+"""
+
+tourInstructions = """
+
+Startup scripts will still be running when your experiment becomes ready.  Watch the "Startup" column on the "List View" tab for your experiment and wait until all of the compute nodes show "Finished" before proceeding.
+
+After all startup scripts have finished...
+
+On `oai-host`, open a terminal session via SSH, or using the shell option for that node in the portal.
+
+"""
+
+UBUNTU_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+COMP_MANAGER_ID = "urn:publicid:IDN+emulab.net+authority+cm"
+
+
+pc = portal.Context()
+
+node_types = [
+    ("d430", "Emulab, d430"),
+]
+
+pc.defineParameter(
+    name="cn_repo_url", 
+    description="GitHub repo for OAI CN5G", 
+    typ=portal.ParameterType.STRING,
+    defaultValue="https://github.com/gulechakan/oai-cn5g-fed"
+    )
+
+pc.defineParameter(
+    name="cn_repo_branch", 
+    description="Branch or commit hash", 
+    typ=portal.ParameterType.STRING, 
+    defaultValue="master"
+    )
+
+pc.defineParameter(
+    name="oai_nodetype",
+    description="Type of compute node to use for OAI node (if included)",
+    typ=portal.ParameterType.STRING,
+    defaultValue=node_types[0],
+    legalValues=node_types
+)
+
+params = pc.bindParameters()
+pc.verifyParameters()
+request = pc.makeRequestRSpec()
+
+# OAI Host
+oai_node = request.RawPC("oai-host")
+oai_node.component_manager_id = COMP_MANAGER_ID
+oai_node.hardware_type = params.cn_nodetype
+oai_node.disk_image = UBUNTU_IMG
+
+# CN5G Startup Script
+deploy_cmd = "/local/repository/bin/deploy-oai-cn5g.sh {} {}".format(params.cn_repo_url, params.cn_repo_branch)
+oai_node.addService(rspec.Execute(shell="bash", command=deploy_cmd))
+
+# Tour
+tour = IG.Tour()
+tour.Description(IG.Tour.MARKDOWN, tourDescription)
+tour.Instructions(IG.Tour.MARKDOWN, tourInstructions)
+request.addTour(tour)
+
+pc.printRequestRSpec(request)
